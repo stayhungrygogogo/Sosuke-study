ce741420c50649a301b6aec173a35152dd7828d4
Sosuke <601599875@qq.com>
2021-04-20 13:52:24 +0800

    demo

---
+++ annotation/src/main/java/com/example/annotationdemo/AnnotationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.annotationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationdemoApplication.class, args);
+    }
+
+}
